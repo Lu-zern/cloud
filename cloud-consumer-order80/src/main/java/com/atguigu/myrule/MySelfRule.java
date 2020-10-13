3d1b648380354cd4f8fde139d8ea48c4dcbac243
lu-zern <ananan231513>
2020-10-13 20:38:57 +0800

    ribbon负载均衡模块

---
+++ cloud-consumer-order80/src/main/java/com/atguigu/myrule/MySelfRule.java
@@ -0,0 +1,19 @@
+package com.atguigu.myrule;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author luzern
+ */
+@Configuration
+public class MySelfRule {
+
+    @Bean
+    public IRule getRandomRule(){
+        return new RandomRule();
+    }
+
+}
--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.atguigu.springcloud.controller;
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,9 +28,29 @@ public class OrderController {
         return restTemplate.postForObject(url+"/payment/create",payment,CommonResult.class);
     }
 
+    @GetMapping(value = "consumer/payment/createForEntity")
+    public CommonResult createForEntity(Payment payment){
+        ResponseEntity<CommonResult> entity = restTemplate.postForEntity(url+"/payment/create",payment,CommonResult.class);
+        if(entity.getStatusCode().is2xxSuccessful()) {
+            return entity.getBody();
+        } else {
+            return new CommonResult<>(444,"操作失败！");
+        }
+    }
+
     @GetMapping(value = "consumer/payment/get/{id}")
     public CommonResult getPaymentById(@PathVariable("id") Long id){
         return restTemplate.getForObject(url+"/payment/get/"+id,CommonResult.class);
+    }
+
+    @GetMapping("/consumer/payment/getForEntity/{id}")
+    public CommonResult getPaymentForEntity(@PathVariable("id") Long id) {
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(url + "/payment/get/" + id, CommonResult.class);
+        if(entity.getStatusCode().is2xxSuccessful()) {
+            return entity.getBody();
+        } else {
+            return new CommonResult<>(444,"操作失败！");
+        }
     }
 
 }
